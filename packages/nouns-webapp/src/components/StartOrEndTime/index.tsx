f039b87ebb9b89739495c8050b609abcd8c39ea6
Phillip Strefling <phillip@strefling.pro>
2023-03-10 10:33:05 -0500

    formatted

--- packages/nouns-webapp/src/components/StartOrEndTime/index.tsx
+++ packages/nouns-webapp/src/components/StartOrEndTime/index.tsx
@@ -1,33 +1,33 @@
-import { Trans } from '@lingui/macro'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import React from 'react'
+import { Trans } from '@lingui/macro';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import React from 'react';
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
 const getCountdownCopy = (startTime: number, endTime: number) => {
-  const startDate = dayjs.unix(startTime)
-  const endDate = dayjs.unix(endTime)
+  const startDate = dayjs.unix(startTime);
+  const endDate = dayjs.unix(endTime);
 
-  const now = dayjs()
+  const now = dayjs();
 
   if (now?.isBefore(startDate)) {
-    return <Trans>starts {endDate.fromNow()}</Trans>
+    return <Trans>starts {endDate.fromNow()}</Trans>;
   } else if (now?.isBefore(endDate)) {
-    return <Trans>ends {dayjs(endDate).fromNow()}</Trans>
+    return <Trans>ends {dayjs(endDate).fromNow()}</Trans>;
   } else {
-    return <Trans>ended {dayjs(endDate).fromNow()}</Trans>
+    return <Trans>ended {dayjs(endDate).fromNow()}</Trans>;
   }
-}
+};
 
 export interface StartOrEndTimeProps {
-  startTime?: number
-  endTime?: number
+  startTime?: number;
+  endTime?: number;
 }
 
-const StartOrEndTime: React.FC = ({
+const StartOrEndTime: React.FC<StartOrEndTimeProps> = ({
   startTime,
   endTime,
-}: StartOrEndTimeProps) => <>{getCountdownCopy(startTime ?? 0, endTime ?? 0)}</>
+}: StartOrEndTimeProps) => <>{getCountdownCopy(startTime ?? 0, endTime ?? 0)}</>;
 
-export default StartOrEndTime
+export default StartOrEndTime;
